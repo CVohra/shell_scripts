54af8eee13d945015b33c698d34801e987f07c1b
Raghu K <raghuk.vit@gmail.com>
2018-06-29 01:49:31 +0000

    Updating shell_scripts

--- single-node-stack.sh
+++ single-node-stack.sh
@@ -7,7 +7,12 @@ rm -f $LOG_FILE
 ### Functions
 DBSetup() {
     yum install mariadb-server -y &>>$LOG_FILE 
-    echo $?
+    if [ $? -eq 0 ]; then 
+        echo "Installing DB Server -- SUCCESS"
+    else
+        echo "Installing DB Server -- FAILURE"
+        exit 1
+    fi
 }
 
 
