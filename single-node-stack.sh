94ecc93d249d5cd49d199051a2af61dc76ca0ec4
Raghu K <raghuk.vit@gmail.com>
2018-06-29 02:03:00 +0000

    Updating shell_scripts

--- single-node-stack.sh
+++ single-node-stack.sh
@@ -12,17 +12,17 @@ N="\e[0m"
 
 ### Functions
 Succ() {
-    echo -e "$1 -- $G SUCCESS $N"
+    echo -e "$1 -- ${G}SUCCESS $N"
 }
 
 Fail() {
-    echo -e "$1 -- $R FAILURE $N"
+    echo -e "$1 -- ${R}FAILURE$N"
     echo "Check the log file -- $LOG_FILE"
     exit 1
 }
 
 Head() {
-    echo -e "\t\e[1;4;36m $1 $N"
+    echo -e "\t\e[1;4;36m$1$N"
 }
 
 Stat() {
@@ -41,4 +41,8 @@ DBSetup() {
 
 
 ### Main Program
+
+## Check Root User or not.
+if [ $(id -u) -ne 0 ]; then 
+    echo -e "${R}"
 DBSetup
